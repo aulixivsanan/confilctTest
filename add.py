cd68b20569a86d28fd17df77bb5d24583cd3f17d
Administrator <18611435237@163.com>
2020-11-08 01:33:04 +0800

    Initial commit

---
+++ add.py
@@ -0,0 +1,16 @@
+#-*- coding     : utf-8 -*-
+# @Time         : 2020/11/8 0:22
+# @Author       : 别拿萌言惑我
+# @File         : add.py
+# @Never Enough : 坚持不一定成功，但放弃必定失败
+
+import unittest
+
+class Add(unittest.TestCase):
+
+    def test01(self):
+        print('1+1')
+
+
+if __name__ == '__main__':
+    unittest.main()
